6a461bb0c67a3c386c529aa6aab91a9f997162bc
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-05-17 23:07:05 +0300

    task: Add comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Set editable background to transparent
 vim.api.nvim_set_hl(0, "OctoEditable", { bold = true, bg = "None" })
+
+-- WSL2 Clipboard Sync
 vim.g.clipboard = {
   name = "WslClipboard",
   copy = {
