854bd6610bd9466ef2f4e50ecad77839beddf5a6
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-05-24 21:42:26 +0300

    feat: Add blamer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Enable Blamer by default
+vim.g.blamer_enabled = 1
+
 -- Set editable background to transparent
 vim.api.nvim_set_hl(0, "OctoEditable", { bold = true, bg = "None" })
 
