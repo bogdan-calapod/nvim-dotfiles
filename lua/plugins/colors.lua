a4244c1e3ae9ccaefc0940cd4e12be182c743e27
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-05-29 17:57:36 +0300

    task: Update colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -7,9 +7,24 @@ return {
     opts = { transparent_background = true },
   },
   {
+    "tomasiser/vim-code-dark",
+    config = function()
+      vim.g.codedark_transparent = 1
+      vim.g.codedark_italics = 1
+      vim.g.codedark_modern = 1
+    end,
+  },
+  {
+    "ray-x/aurora",
+    config = function()
+      vim.g.aurora_italic = 1
+      vim.g.aurora_transparent = 1
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "catppuccin",
     },
   },
 }
