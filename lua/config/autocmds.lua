e5cf9bfde97853cecb5aa5b52307664b1088c94e
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-05-30 12:07:47 +0300

    feat: add python formatting with black

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Run black % on save on python files
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.py" },
+  command = "!black %",
+})
