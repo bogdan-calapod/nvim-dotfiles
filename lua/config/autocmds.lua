1a362a35acc1069fb56884f572461637ccda4266
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-06-20 11:55:57 +0300

    task: Remove autopep8 hack
    This was a hack that ran the `autopep8` executable with in-place instead of integrating with null-ls like it should

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,3 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-
--- Run black % on save on python files
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*.py" },
-  command = "!autopep8 --in-place --aggressive --aggressive %",
-})
