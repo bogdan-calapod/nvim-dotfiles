0f947f814f5ba1ddfdf8a7bd628d4a6475f7f548
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-21 10:30:30 +0300

    task: Add options for code actions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,7 +19,9 @@ return {
       "nvim-lua/plenary.nvim",
       "neovim/nvim-lspconfig",
     },
-    opts = {}
+    opts = {
+      expose_as_code_action = { 'fix_all', 'add_missing_imports', 'remove_unused' }
+    }
   },
   {
     "Wansmer/treesj",
