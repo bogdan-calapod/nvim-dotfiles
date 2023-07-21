12f4b462fe86eaae1ae9fd534f596fea4578ba29
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-07-21 10:30:07 +0300

    feat: Add local fs option to octo

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -7,6 +7,9 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("octo").setup()
+    require("octo").setup({
+      use_local_fs = true,
+      enable_builtin = true
+    })
   end,
 }
