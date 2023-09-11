bb606cff93bf3bc980064da54899fb4b4af34822
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-09-11 13:42:05 +0300

    task: Reorder git plugins

--- lua/plugins/gitstuff.lua
+++ lua/plugins/gitstuff.lua
@@ -8,6 +8,21 @@ return {
     lazy = false
   },
   {
+    "pwntester/octo.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({
+        use_local_fs = true,
+        enable_builtin = true
+      })
+    end,
+  },
+  {
     "ldelossa/gh.nvim",
     lazy = false,
     dependencies = { { "ldelossa/litee.nvim" } },
