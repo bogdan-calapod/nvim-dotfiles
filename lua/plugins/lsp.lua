a89c9577f751d62209cada2b4e73369f1d2fc602
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-10-13 15:19:11 +0300

    task: Update lazy config
    Updated to v10, removed manual extras

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -65,13 +65,6 @@ return {
     },
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
