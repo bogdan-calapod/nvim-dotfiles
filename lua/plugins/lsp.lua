4cb7ef6dd88c1cd78a3f9e3b25146972af3460a4
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-10-26 18:37:19 +0300

    feat: Add sonarlint

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,18 @@
 
 return {
   {
-    "dgagn/diagflow.nvim", opts = { scope = 'line' }
+    "dgagn/diagflow.nvim",
+    opts = { scope = "line" },
+  },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("refactoring").setup()
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
@@ -26,10 +37,12 @@ return {
   {
     "Wansmer/treesj",
     keys = {
-      '<space>m',
+      "<space>m",
     },
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function() require('treesj').setup({}) end
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup({})
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -62,6 +75,35 @@ return {
         "pyright",
         "autopep8",
       },
+    },
+    {
+      url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+      ft = { "python", "typescript", "typescriptreact", "typescript.tsx" },
+      dependencies = {
+        "mfussenegger/nvim-jdtls",
+        "williamboman/mason.nvim",
+      },
+      config = function()
+        require("sonarlint").setup({
+          server = {
+            cmd = {
+              "sonarlint-language-server",
+              -- Ensure that sonarlint-language-server uses stdio channel
+              "-stdio",
+              "-analyzers",
+              -- paths to the analyzers you need, using those for python and java in this example
+              vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+              vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjs.jar"),
+            },
+          },
+          filetypes = {
+            "python",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+        })
+      end,
     },
   },
   {
