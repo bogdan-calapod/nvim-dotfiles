8620af2febc8f1f9362e2b7c7fa7cd7b6bff9db5
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-10-31 09:31:55 +0200

    task: Bump versions

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,17 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
-      -- Disable transparent floats because it borks Octo - should be fixed soon-ish based on Octo's commit history
-      styles = { sidebars = "transparent" } -- , floats = "transparent" },
-    }
+      -- -- Disable transparent floats because it borks Octo - should be fixed soon-ish based on Octo's commit history
+      styles = {
+        sidebars = "transparent",
+        -- floats = "transparent",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight",
-    }
+    },
   },
 }
