aeb1886628f93d3839b8e7e66cccb1306bb3bd50
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-11-09 22:06:09 +0200

    task: Improve line number color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,10 @@ return {
         sidebars = "transparent",
         -- floats = "transparent",
       },
+      on_highlights = function(hl, colors)
+        hl.LineNr = { bg = "none", fg = "#999999" }
+        hl.CursorLineNr = { fg = "#ffffff" }
+      end,
     },
   },
   {
