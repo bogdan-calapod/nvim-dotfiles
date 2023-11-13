36234228c27b0dee8a7b8a9a54677cb20850dca9
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-11-13 13:54:51 +0200

    task: Add back sonarlint
    In the hopes that it will get fixed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -44,7 +44,37 @@ return {
         "prettierd",
         "css-lsp",
         "pyright",
+        "black"
       },
     },
+  },
+  {
+    url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+    ft = { "python", "typescript", "typescriptreact", "typescript.tsx" },
+    dependencies = {
+      "mfussenegger/nvim-jdtls",
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("sonarlint").setup({
+        server = {
+          cmd = {
+            "sonarlint-language-server",
+            -- Ensure that sonarlint-language-server uses stdio channel
+            "-stdio",
+            "-analyzers",
+            -- paths to the analyzers you need, using those for python and java in this example
+            vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+            vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjs.jar"),
+          },
+        },
+        filetypes = {
+          "python",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+      })
+    end,
   },
 }
