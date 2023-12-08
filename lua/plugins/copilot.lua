1f429e4aa971a51b420cca9d3838e0717fee1e9f
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-12-08 07:58:36 +0200

    feat: Add custom copilot options

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = false,
+    },
+    panel = {
+      enabled = true,
+      auto_refresh = true,
+    }
+  }
+}
