4c262b8cc54a68254d5be01047f9716b634522c8
Bogdan Calapod <bogdan.calapod@pdq.com>
2024-09-09 18:02:59 +0300

    feat: Add catpuccin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -53,6 +53,7 @@ return {
         "bash-language-server",
         "prettierd",
         "css-lsp",
+        "ruff",
         "pyright",
         "autopep8",
         "shellcheck",
@@ -92,5 +93,14 @@ return {
         },
       })
     end,
+  },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    opts = {},
+    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
   },
 }
