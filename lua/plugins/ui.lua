d933b3e4d460dadcb28259406e5adbcc675d49d4
Bogdan Calapod <bogdan.calapod@pdq.com>
2024-12-28 11:26:59 +0100

    feat: Add breadcrumb

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,4 +2,35 @@ return {
   { "rasulomaroff/reactive.nvim" },
   { "HiPhish/rainbow-delimiters.nvim" },
   { "shortcuts/no-neck-pain.nvim", version = "*", opts = { width = 200, autocmds = { enableOnVimEnter = true } } },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {},
+    config = function()
+      require("barbecue").setup({
+        create_autocmd = false, -- prevent barbecue from updating itself automatically
+        symbols = { separator = "󰅂" },
+      })
+
+      vim.api.nvim_create_autocmd({
+        "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+        "BufWinEnter",
+        "CursorHold",
+        "InsertLeave",
+
+        -- include this if you have set `show_modified` to `true`
+        -- "BufModifiedSet",
+      }, {
+        group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+        callback = function()
+          require("barbecue.ui").update()
+        end,
+      })
+    end,
+  },
 }
